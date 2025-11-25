2079bafde0b96ce29f0b875e20421e481eb2fabc
Falcon23956 <kioliemmanuel@gmail.com>
2025-11-25 09:35:46 +0300

    Updated brand name to CRESTVISTA PROPERTIES LTD

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,8 @@ export default function Navbar(){
   return (
     <nav>
       <div className="inner container">
-        <Link href="/" className="logo"><img src="/logo.png" alt="logo"/><span style={{fontWeight:700,color:'#0A3125'}}>CRESTVISTA</span></Link>
+        <Link href="/" className="logo"><img src="/logo.png" alt="logo"/><span style={{fontWeight:700,color:'#0A3125'}}<div className="logo">CRESTVISTA <span className="handwrite">PROPERTIES LTD</span></div>
+/span></Link>
         <div className="nav-links" aria-hidden>
           <Link href="/properties">Properties</Link>
           <Link href="/services">Services</Link>
