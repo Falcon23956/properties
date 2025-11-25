9c3d8d7169166afcad1884a8b6fb399d0362d74f
Falcon23956 <kioliemmanuel@gmail.com>
2025-11-25 07:23:18 +0300

    fix: typed property in PropertyCard

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -1,9 +1,33 @@
-export default function PropertyCard({ property }: { property: any }) {
+type Property = {
+  id: string;
+  title: string;
+  summary: string;
+  image: string;
+};
+
+type Props = {
+  property: Property;
+};
+
+export default function PropertyCard({ property }: Props) {
+  return (
     <div className="card">
-      <div style={{height:160,overflow:'hidden',borderRadius:8,background:'#eee'}}><img src={property.image} alt={property.title} style={{width:'100%',height:'100%',objectFit:'cover'}}/></div>
-      <h3 style={{marginTop:10}}>{property.title}</h3>
-      <p style={{color:'#5a4e2e'}}>{property.location}</p>
-      <p style={{fontWeight:700}}>{property.price}</p>
+      <div
+        style={{
+          height: 160,
+          overflow: "hidden",
+          borderRadius: 8,
+          background: "#eee",
+        }}
+      >
+        <img
+          src={property.image}
+          alt={property.title}
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+      </div>
+
+      <h3>{property.title}</h3>
       <p>{property.summary}</p>
     </div>
   );
