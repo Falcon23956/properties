bf3ca8c6f7f45903b849b7e99fd017411d8824f3
Falcon23956 <kioliemmanuel@gmail.com>
2025-11-25 07:16:27 +0300

    fix: typed property in PropertyCard

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -1,4 +1,3 @@
-
 export default function PropertyCard({ property }: { property: any }) {
     <div className="card">
       <div style={{height:160,overflow:'hidden',borderRadius:8,background:'#eee'}}><img src={property.image} alt={property.title} style={{width:'100%',height:'100%',objectFit:'cover'}}/></div>
